46ae9ae04f93d8797ccc0ac97d4167686bb01b12
James Walshe <jameswalshe@kainos.com>
2022-11-22 17:14:31 +0000

    Added get roles endpoint

---
+++ src/main/java/com/kainos/ea/model/JobRole.java
@@ -0,0 +1,10 @@
+package com.kainos.ea.model;
+
+import com.kainos.ea.dao.JobRoleDAO;
+
+public class JobRoleService {
+
+    public List<JobRoleDAO> getJobRoles(){
+        return
+    }
+}
---
+++ src/main/java/com/kainos/ea/service/JobRoleService.java
@@ -0,0 +1,2 @@
+package com.kainos.ea.service;public class JobRoleService {
+}
