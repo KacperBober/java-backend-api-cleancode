9667d2628b24f5d5b567fa4ee867228d890dea5e
Kacper Bober <kacperbober112@gmail.com>
2022-11-24 11:48:29 +0100

    created JobSpec model class

---
+++ src/main/java/com/kainos/ea/model/JobSpec.java
@@ -0,0 +1,17 @@
+package com.kainos.ea.model;
+
+public class JobSpec {
+    private String job_spec;
+
+    public JobSpec(String job_spec) {
+        this.job_spec = job_spec;
+    }
+
+    public String getJob_spec() {
+        return job_spec;
+    }
+
+    public void setJob_spec(String job_spec) {
+        this.job_spec = job_spec;
+    }
+}
